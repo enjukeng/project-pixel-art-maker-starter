f4cfd6bcee47b411be3c600762515d5a7d505f33
enjukeng <118087916+enjukeng@users.noreply.github.com>
2022-12-19 01:15:56 -0700

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,25 @@
 // Select color input
+const colorInput = document.querySelector('#colorPicker');
+colorInput.addEventListener('click', function() {
+    console.log('Grid square color changed!');
+});
 // Select size input
+const table = document.getElementById('pixelCanvas')
+const height = document.querySelector('#inputHeight').value;
+const width = document.querySelector('#inputWidth').value;
+const gridSize = document.getElementById('sizePicker');
+gridSize.addEventListener('click', function() {
+    console.log('Grid square size changed!');
+});
+
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
-}
+function makeGrid(height, width) {
+    for (var N = 0; N <= height - 1; N++) {
+        console.log(table.insertRow);
+        for (var M = 0; M <= width - 1; M++) {
+            console.log(table.insertRow.insertCell);
+        }
+    }
+};
